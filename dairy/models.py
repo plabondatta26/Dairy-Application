bad80924d1f88db988eb45c20f876b5a5db2960b
Plabon Datta <plabondatta26@gmail.com>
2020-09-07 20:30:14 +0600

    Dairy completed

--- dairy/models.py
+++ dairy/models.py
@@ -7,6 +7,6 @@ class Dairy(models.Model):
     title= models.CharField(max_length=100, blank=False)
     description = models.TextField(blank=False)
     created_on = models.DateTimeField(auto_now=True)
-    user= models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
