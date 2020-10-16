f4d10c05bcff5f16bbf89c47c6f536d7f15c5568
Plabon Datta <plabondatta26@gmail.com>
2020-10-16 19:50:00 +0600

    redy to deploy

--- Dairy_with_login/urls.py
+++ Dairy_with_login/urls.py
@@ -5,5 +5,5 @@ import dairy, users
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('dairy.urls')),
-    path('',include('users.urls')),
+    path('user/',include('users.urls')),
 ]
