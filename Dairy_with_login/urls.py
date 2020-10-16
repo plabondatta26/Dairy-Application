6cced66ab8735d2aba481606376a446a9f7c7ca5
Plabon Datta <plabondatta26@gmail.com>
2020-10-16 19:59:06 +0600

    redy to deploy

--- Dairy_with_login/urls.py
+++ Dairy_with_login/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-import dairy, users
-
+from dairy import views as dairy
+from users import views as user
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('dairy.urls')),
-    path('user/',include('users.urls')),
+
+    path('', dairy.index, name='home'),
+    path('dashboard/', dairy.dashboard, name='dashboard'),
+    path('add/', dairy.Add_new, name='add'),
+    path('details/<int:id>/', dairy.Details, name='details'),
+    path('edit/<int:id>/', dairy.Edit, name='edit'),
+    path('delete/<int:id>/', dairy.Delete, name='delete'),
+    path('pdf/<int:id>/', dairy.pdfGenerator, name='pdf'),
+    path('make/<int:id>/', dairy.pdfView, name='pdfView'),
+
+    path('register/', user.registration, name ='register'),
+    path('login/', user.login_page, name='login'),
+    path('logout/', user.logout_page, name='logout'),
 ]
