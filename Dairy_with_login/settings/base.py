1e54ea930e9a6e2173d57f3920e3ec0bc98dd33c
Plabon Datta <plabondatta26@gmail.com>
2020-10-16 19:00:04 +0600

    ready to deploy

--- Dairy_with_login/settings/base.py
+++ Dairy_with_login/settings/base.py
@@ -110,4 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
